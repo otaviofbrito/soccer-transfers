ff50d5177cdde49723bd423bbb83d70a05596081
Otavio F <otaviofbrito@gmail.com>
2024-08-14 23:15:32 -0300

    in degrees

--- src/utils/sql/queries.py
+++ src/utils/sql/queries.py
@@ -1,3 +1,8 @@
 queries = {
-    'get_all_clubs': 'SELECT * FROM clubs'
+    'get_all_clubs': "SELECT * FROM clubs",
+    'get_all_not_loan_transfers': """ 
+                                      SELECT * FROM transfers t WHERE transfer_type = 'Not loan'
+                                      AND left_club_id NOT IN (-5, 75, 515)
+                                      AND joined_club_id NOT IN (-5, 75, 515)
+                                    """
 }
